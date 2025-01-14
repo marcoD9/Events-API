16a3d57ec09163debb58ad73d809a28531ffc13f
Marco <marco.dmail96@gmail.com>
2025-01-14 17:02:05 +0100

    Add getEventById

---
+++ src/services/events/getEventById.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const getEventById = (id) => {
+  const filePath = path.join(__dirname, "..", "..", "data", "events.json");
+  // Leggi il file JSON
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  // Parsea il JSON
+  const eventsData = JSON.parse(jsonData);
+
+  // Ottieni l'evento
+  const event = eventsData.events.find((event) => event.id === id);
+
+  if (!event) {
+    throw new Error();
+  }
+
+  return event;
+};
+
+module.exports = getEventById;
