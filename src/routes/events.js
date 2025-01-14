22a3583f25cf81219aa1ee4582d22921132f7414
Marco <marco.dmail96@gmail.com>
2025-01-14 17:11:42 +0100

    Update events.js router

--- src/routes/events.js
+++ src/routes/events.js
@@ -2,6 +2,7 @@ const express = require("express");
 const getEvents = require("../services/events/getEvents.js");
 const createEvent = require("../services/events/createEvent.js");
 const getEventById = require("../services/events/getEventById.js");
+const updateEvent = require("../services/events/updateEvent.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -38,14 +39,33 @@ router.get("/:id", (req, res) => {
   const event = getEventById(id);
   res.status(200).json(event);
 });
-/*
+
 router.put("/:id", (req, res) => {
   const { id } = req.params;
-  const { username, password, name, image } = req.body;
-  const updatedUser = updateUser(id, username, password, name, image);
-  res.status(200).json(updatedUser);
+  const {
+    title,
+    description,
+    location,
+    image,
+    startTime,
+    endTime,
+    createdBy,
+    categoryIds,
+  } = req.body;
+  const updatedEvent = updateEvent(
+    id,
+    title,
+    description,
+    location,
+    image,
+    startTime,
+    endTime,
+    createdBy,
+    categoryIds
+  );
+  res.status(200).json(updatedEvent);
 });
-
+/*
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
   const deletedUserId = deleteUser(id);
