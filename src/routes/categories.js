35fff00a4dd886d6a935227c58e2b4ad8965274c
Marco <marco.dmail96@gmail.com>
2025-01-14 19:05:34 +0100

    Add categories.js router

---
+++ src/routes/categories.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const getCategories = require("../services/categories/getCategories");
+const createCategory = require("../services/categories/createCategory");
+const getCategoryById = require("../services/categories/getCategoryById");
+const updateCategory = require("../services/categories/updateCategory");
+const deleteCategory = require("../services/categories/deleteCategory");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const users = getCategories();
+  res.status(200).json(users);
+});
+
+router.post("/", (req, res) => {
+  const { name } = req.body;
+  const newCategory = createCategory(name);
+  res.status(201).json(newCategory);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const category = getCategoryById(id);
+  res.status(200).json(category);
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const updatedCategory = updateCategory(id, name);
+  res.status(200).json(updatedCategory);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deletedCategoryId = deleteCategory(id);
+
+  res.status(200).json({
+    message: `Category with id ${deletedCategoryId} was deleted!`,
+  });
+});
+
+module.exports = router;
