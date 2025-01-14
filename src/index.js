4d78c3c93b513982acb9a2be2828688693bffb39
Marco <marco.dmail96@gmail.com>
2025-01-14 19:45:42 +0100

    Update index.js, now a user can login

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // Require this first!
 require("./instrument");
 // Now require other modules
+require("dotenv/config");
 const express = require("express");
 const Sentry = require("@sentry/node");
 const usersRouter = require("./routes/users.js");
 const eventsRouter = require("./routes/events.js");
 const categoriesRouter = require("./routes/categories.js");
+const loginRouter = require("./routes/login.js");
 
 const app = express();
 app.use(express.json());
@@ -17,6 +19,8 @@ app.get("/", (req, res) => {
 app.use("/users", usersRouter);
 app.use("/events", eventsRouter);
 app.use("/categories", categoriesRouter);
+app.use("/login", loginRouter);
+
 // Add this after all routes,
 // but before any and other error-handling middlewares are defined
 Sentry.setupExpressErrorHandler(app);
