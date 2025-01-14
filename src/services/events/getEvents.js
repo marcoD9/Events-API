a0c38ffa269f9b1ffc2a2e5e062a6ed4f3583f45
Marco <marco.dmail96@gmail.com>
2025-01-14 16:29:14 +0100

    Add getEvents

---
+++ src/services/events/getEvents.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const getEvents = () => {
+  // Risolve il percorso a partire dalla cartella 'src/services/users/'
+  const filePath = path.join(__dirname, "..", "..", "data", "events.json");
+
+  // Leggi il file JSON
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+
+  // Parsea il JSON
+  const eventsData = JSON.parse(jsonData);
+
+  // Ottieni gli utenti
+  let events = eventsData.events;
+
+  return events;
+};
+
+module.exports = getEvents;
