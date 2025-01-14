15967d530afaea664de7be882a3962a8a9dee74b
Marco <marco.dmail96@gmail.com>
2025-01-14 16:28:02 +0100

    Add updateUser

---
+++ src/services/users/updateUser.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const updateUser = (id, username, password, name, image) => {
+  const filePath = path.join(__dirname, "..", "..", "data", "users.json");
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const usersData = JSON.parse(jsonData);
+  const user = usersData.users.find((user) => user.id === id);
+
+  if (!user) {
+    throw new Error("User not found", error);
+  }
+
+  user.username = username ?? user.username;
+  user.password = password ?? user.password;
+  user.name = name ?? user.name;
+  user.image = image ?? user.image;
+  return user;
+};
+
+module.exports = updateUser;
