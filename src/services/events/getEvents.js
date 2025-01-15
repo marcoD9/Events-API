71f8d0db1c71271ba604cabb8291e7918178831a
Marco <marco.dmail96@gmail.com>
2025-01-15 11:34:22 +0100

    Update getEvents to accept query

--- src/services/events/getEvents.js
+++ src/services/events/getEvents.js
@@ -1,20 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-const getEvents = () => {
+const getEvents = (title, location) => {
   // Risolve il percorso a partire dalla cartella 'src/services/events/'
   const filePath = path.join(__dirname, "..", "..", "data", "events.json");
-
   // Leggi il file JSON
   const jsonData = fs.readFileSync(filePath, "utf-8");
-
   // Parsea il JSON
   const eventsData = JSON.parse(jsonData);
-
   // Ottieni gli Eventi
-  let events = eventsData.events;
-
-  return events;
+  return eventsData.events.filter((event) => {
+    return (
+      (!title || event.title === title) &&
+      (!location || event.location === location)
+    );
+  });
 };
 
 module.exports = getEvents;
